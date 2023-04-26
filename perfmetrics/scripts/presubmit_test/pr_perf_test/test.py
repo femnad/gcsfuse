bd6ca8d18a3437d379fa32f3845e7983e959be4f
Tulsi Shah <tulsishah@google.com>
2023-04-26 09:38:08 +0000

    local testing

--- perfmetrics/scripts/presubmit_test/pr_perf_test/test.py
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/test.py
@@ -1,6 +1,7 @@
 import os
 
 if __name__ == '__main__':
+  print("ENV VARIABLE: ",os.getenv('JOB_TYPE'))
   if os.getenv('JOB_TYPE') == "CONTINUOUS_INTEGRATION" :
     print("Tulsi")
   print("NOT EXECUTE.. :(")
