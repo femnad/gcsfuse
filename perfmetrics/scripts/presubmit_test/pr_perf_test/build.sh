8d3b2b3b3f0efb60bf528c5b83e140d7a759ac7c
Tulsi Shah <tulsishah@google.com>
2023-04-26 09:16:43 +0000

    local testing

--- perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
+++ perfmetrics/scripts/presubmit_test/pr_perf_test/build.sh
@@ -6,4 +6,4 @@ echo Installing git
 sudo apt-get install git
 echo Installing python3-pip
 sudo apt-get -y install python3-pip
-python3 ./test.py
+python3 perfmetrics/scripts/presubmit_test/pr_perf_test/test.py
